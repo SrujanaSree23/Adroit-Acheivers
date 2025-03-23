8556e9718b86e135cc68c96da537c0949c7d8719
Srujana Sree K V <srujanasree230705@gmail.com>
2025-03-23 10:19:29 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,12 @@
+function goToPage(page) {
+    window.location.href = page;
+}
+
+function toggleChatbox() {
+    let chatbox = document.getElementById("chatbox");
+    chatbox.style.display = chatbox.style.display === "flex" ? "none" : "flex";
+}
+
+function searchFunction() {
+    alert("Search functionality coming soon!");
+}
